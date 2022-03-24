1b3b28eef3883c3a94e415119f3e9787112c70f9
minerva421 <minerva421.ai@gmail.com>
2022-03-24 13:06:49 +0900

    RoutedEvent 작성중

---
+++ DotnetSkoool/DotnetSkoool/RoutedEventDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEventDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OuterButton_Click(object sender, RoutedEventArgs e)  // 버블링 이벤트
+        {
+            MessageBox.Show("Outer Button Click!");
+        }
+
+        private void outerEllipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Green Ellipse Click!");
+
+        }
+    }
+}
